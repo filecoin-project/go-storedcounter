f60c559f62a7316eba96975f5502af77d2e2c735
hannahhoward <hannah@hannahhoward.net>
2020-04-21 12:48:30 -0700

    fix(storedcounter): add mutex
    Add mutex to insure atomic ops

--- storedcounter.go
+++ storedcounter.go
@@ -2,24 +2,29 @@ package storedcounter
 
 import (
 	"encoding/binary"
+	"sync"
 
 	"github.com/ipfs/go-datastore"
 )
 
 // StoredCounter is a counter that persists to a datastore as it increments
 type StoredCounter struct {
+	lock sync.Mutex
 	ds   datastore.Datastore
 	name datastore.Key
 }
 
 // New returns a new StoredCounter for the given datastore and key
 func New(ds datastore.Datastore, name datastore.Key) *StoredCounter {
-	return &StoredCounter{ds, name}
+	return &StoredCounter{ds: ds, name: name}
 }
 
 // Next returns the next counter value, updating it on disk in the process
 // if no counter is present, it creates one and returns a 0 value
 func (sc *StoredCounter) Next() (uint64, error) {
+	sc.lock.Lock()
+	defer sc.lock.Unlock()
+
 	has, err := sc.ds.Has(sc.name)
 	if err != nil {
 		return 0, err
